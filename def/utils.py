9ff5d83b44a2f81ba4dd594c86631b2e232121de
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-09-06 12:22:02 +0530

    Update utils.py

--- def/utils.py
+++ def/utils.py
@@ -1,7 +1,7 @@
 async def mention(bot, id):
     a = await bot.get_entity(int(id))
     x = a.first_name
-    return [{x}](tg://user?id={id})
+    return f'[{x}](tg://user?id={id})'
     
 def name(x):
     file = x.media
