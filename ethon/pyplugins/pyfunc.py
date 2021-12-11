9773c2d5458e855caab5458f74ba32a29ee80f7e
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-11 11:33:42 +0530

    Update pyfunc.py

--- ethon/pyplugins/pyfunc.py
+++ ethon/pyplugins/pyfunc.py
@@ -1,4 +1,11 @@
 import subprocess 
+import cv2
+
+#fastest way to get total number of frames in a video
+def total_frames(video_path):
+    cap = cv2.VideoCapture(f"{video_path}")
+    tf = int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) 
+    return tf        
 
 def bash(cmd):    
     bashCommand = f"{cmd}"
