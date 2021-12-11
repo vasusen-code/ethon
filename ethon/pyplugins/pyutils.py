10f2bd6cace3af6523842284d8fcc52fa439810f
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-11 11:16:04 +0530

    Update pyutils.py

--- ethon/pyplugins/pyutils.py
+++ ethon/pyplugins/pyutils.py
@@ -7,3 +7,13 @@ import pathlib
 def file_extension(file_path):
     extension = pathlib.Path(f'{file_path}').suffix 
     return extension
+
+#make a list with limited length and check length
+#Limit = maximun length of the list
+def Q_length(List, limit):
+    length = len(List)
+    if length > int(limit) + 1:
+        return 'FULL'
+    else:
+        return length
+    
