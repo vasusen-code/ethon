06787fa9b278c9592bd4f9d7e236f7b89593edd5
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-11 11:42:30 +0530

    Update pyutils.py

--- ethon/pyplugins/pyutils.py
+++ ethon/pyplugins/pyutils.py
@@ -1,5 +1,16 @@
-#DroneBots/Vasusen-code/Maahi/shah/
-#__ChauhanMahesh__
+"""This file is part of the CompressorBot distribution.
+Copyright (c) 2021 vasusen-code
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3.
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+License can be found in < https://github.com/vasusen-code/ethon/blob/main/LICENSE > ."""
+
+#vasusen-code/thechariotoflight/dronebots
+#__TG:ChauhanMahesh__
 
 import pathlib
 
