e7fb0b7211f9f2da0f74470621f1e44fcfe867ad
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-11 11:38:09 +0530

    Update pyfunc.py

--- ethon/pyplugins/pyfunc.py
+++ ethon/pyplugins/pyfunc.py
@@ -1,3 +1,18 @@
+"""This file is part of the CompressorBot distribution.
+Copyright (c) 2021 vasusen-code
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+License can be found in < https://github.com/1Danish-00/CompressorBot/blob/main/License> ."""
+
+
 import subprocess 
 import cv2
 
