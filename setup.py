b11b004c494b25e5052aef9552a47a2dd8fb8018
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-12 10:04:58 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,48 @@
+import re
+
+import setuptools
+
+with open("ethon/version.py", "rt", encoding="utf8") as x:
+    version = re.search(r'__version__ = "(.*?)"', x.read()).group(1)
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+name = "ethon"
+author = "vasusen-code"
+author_email = "maahisnu144@gmail.com"
+description = "Package containing basic functions to build telegram bots."
+license = "GNU AFFERO GENERAL PUBLIC LICENSE (v3)"
+url = "https://github.com/vasusen-code/ethon"
+classifiers = [
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "Operating System :: OS Independent",
+]
+requirements = [
+    "python-decouple"
+    "telethon"
+    "aiofiles",
+    "aiohttp"
+]
+
+
+setuptools.setup(
+    name=name,
+    version=version,
+    author=author,
+    author_email=author_email,
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=url,
+    license=license,
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=classifiers,
+    python_requires=">=3.6",
+)
