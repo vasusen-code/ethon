3e8d6f60c7be6c84414b816691e12e2b59f6fa5a
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-12 11:17:44 +0530

    Update version.py

--- ethon/version.py
+++ ethon/version.py
@@ -1,3 +1,12 @@
-__version__= "2021.12.12"
+import os
 
-ethon_version = "0.0.1"
+
+def string():
+    try:
+        with open(os.path.dirname(__file__) + "/VERSION", "r", encoding="utf-8") as fh:
+            version = fh.read().strip()
+            if version:
+                return version
+    except:
+        pass
+    return "unknown (git checkout)"
