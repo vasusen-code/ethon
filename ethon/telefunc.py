67b1d88e49cb66203612c1dab6cb6fe96a0fcab1
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-14 21:19:15 +0530

    Update telefunc.py

--- ethon/telefunc.py
+++ ethon/telefunc.py
@@ -53,7 +53,7 @@ async def progress(current, total, event, start, type_of_ps, file=None):
         )
         tmp = (
             progress_str
-            + "{0} of {1}\n\nSpeed: {2}/s\n\nETA: {3}\n\n".format(
+            + "GROSS: {0} of {1}\n\nSpeed: {2}/s\n\nETA: {3}\n\n".format(
                 hbs(current),
                 hbs(total),
                 hbs(speed),
