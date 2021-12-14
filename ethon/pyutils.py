8191fb34682e0552bdebec1b381731bcf99ef003
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-14 19:19:25 +0530

    Update pyutils.py

--- ethon/pyutils.py
+++ ethon/pyutils.py
@@ -1,4 +1,4 @@
-"""This file is part of the CompressorBot distribution.
+"""This file is part of the ethon distribution.
 Copyright (c) 2021 vasusen-code
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
