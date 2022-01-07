df2ce20727b82f04d0e59d03fd3719b53324e5b6
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-07 09:18:49 +0530

    Update uploader.py

--- ethon/uploader.py
+++ ethon/uploader.py
@@ -3,36 +3,40 @@ import requests
 import re
 import os
 import subprocess
+import asyncio
 
-def bash(cmd):    
-    bashCommand = f"{cmd}"
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE) 
-    output, error = process.communicate()
-    return output, error
-
+async def bash(cmd):
+    cmd_ = cmd.split()
+    process = await asyncio.create_subprocess_exec(*cmd_, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+    stdout, stderr = await process.communicate() 
+    e = stderr.decode().strip()
+    o = stdout.decode().strip()
+    print(o)
+    print(e)
+    
 #Download videos from youtube-------------------------------------------------------------------------------------------
 def download_from_youtube(url):
-    bash(f'yt-dlp -f best --no-warnings -o ./yt/%(title)s.%(ext)s {url}')
+    await bash(f'yt-dlp -f best --no-warnings --prefer-ffmpeg -o %(title)s.%(ext)s {url}')
     with yt_dlp.YoutubeDL({}) as ydl:
         info = ydl.extract_info(url, download=False)
         video_title = info.get("title", None)
         video_ext = info.get("ext", None) 
-        return "./yt/" + video_title + "." + video_ext
+        return f"{video_title}.{video_ext}"
     
 #for ytdlp supported sites ------------------------------------------------------------------------------------------
 
 def ytdl(url):
     if 'HLS' in url:
-        bash(f'yt-dlp -f best --no-warnings --hls-prefer-ffmpeg -o ./yt/%(title)s.%(ext)s {url}')
+        await bash(f'yt-dlp -f best --no-warnings --hls-prefer-ffmpeg -o %(title)s.%(ext)s {url}')
     elif 'm3u8' in url:
-        bash(f'yt-dlp -f best --no-warnings --hls-prefer-ffmpeg -o ./yt/%(title)s.%(ext)s {url}')
+        await bash(f'yt-dlp -f best --no-warnings --hls-prefer-ffmpeg -o %(title)s.%(ext)s {url}')
     else:
-        bash(f'yt-dlp -f best --no-warnings -o ./yt/%(title)s.%(ext)s {url}')
+        await bash(f'yt-dlp -f best --prefer-ffmpeg --no-warnings -o %(title)s.%(ext)s {url}')
     with yt_dlp.YoutubeDL({}) as ydl:
         info_dict = ydl.extract_info(url, download=False)
         video_title = info_dict.get('title', None) 
         video_ext = info_dict.get('ext', None) 
-        return "./yt/" + video_title + "." + video_ext
+        return f"{video_title}.{video_ext}"
     
 #weburl download------------------------------------------------------------------------------
 
