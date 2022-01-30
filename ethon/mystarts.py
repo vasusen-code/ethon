d51c9d537a87688f9c2ef9cde88cb73b3b94e54c
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-30 12:02:21 +0530

    Update mystarts.py

--- ethon/mystarts.py
+++ ethon/mystarts.py
@@ -6,8 +6,8 @@ from telethon import events, Button
 async def start_srb(event):
     await event.reply("Send me Link of any message to clone it here, For private channel message, send invite link first.\n\n**SUPPORT:** @TeamDrone", 
                       buttons=[
-                              [Button.inline("SET THUMB.", data="sett"),
-                               Button.inline("REM THUMB.", data="remt")],
+                              [Button.inline("SET THUMB.", data="set"),
+                               Button.inline("REM THUMB.", data="rem")],
                               [Button.url("DEV", url="t.me/MaheshChauhan")]])
                               
     
