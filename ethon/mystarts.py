2ed081ec72bb7f9fde7da87d5d65205f2b3459db
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-30 11:21:42 +0530

    Create mystarts.py

---
+++ ethon/mystarts.py
@@ -0,0 +1,13 @@
+#ignore this file
+
+from telethon import events
+
+
+async def start_srb(event):
+    await event.reply("Send me Link of any message to clone it here, For private channel message, send invite link first.\n\n**SUPPORT:** @TeamDrone", 
+                      buttons=[
+                              [Button.inline("SET THUMB.", data="sett"),
+                               Button.inline("REM THUMB.", data="remt")],
+                              [Button.url("DEV", url="t.me/MaheshChauhan")]])
+                              
+    
