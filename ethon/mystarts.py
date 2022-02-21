8f101fa9252b7e4c010b2fbfacd482e62d95a6f7
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-02-21 10:02:37 +0530

    Update mystarts.py

--- ethon/mystarts.py
+++ ethon/mystarts.py
@@ -11,3 +11,12 @@ async def start_srb(event, st):
                               [Button.url("DEV", url="t.me/MaheshChauhan")]])
                               
     
+async def vc_menu(event):
+    await event.edit("**VIDEO CONVERTOR v1.4**", 
+                    buttons=[
+                        [Button.inline("info.", data="info"),
+                         Button.inline("SOURCE", data="source")],
+                        [Button.inline("NOTICE.", data="notice"),
+                         Button.inline("Main.", data="help")],
+                        [Button.url("DEVELOPER", url="t.me/MaheshChauhan")]])
+    
