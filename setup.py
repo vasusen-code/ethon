7265cdf4ecc5f204185ddcdd4b777246ea473358
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-05-09 10:45:14 +0530

    Update for VC

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import re
 import os
 import setuptools
 
-ver = 'v1.3.7'
+ver = 'v1.3.8'
 
 with open("README.rst", "r", encoding="utf-8") as fh:
     long_desc = fh.read()
@@ -22,10 +22,7 @@ classify = [
 requirements = [
     "python-decouple",
     "telethon",
-    "aiofiles",
-    "aiohttp",
     "opencv-python-headless",
-    "yt_dlp",
     "requests",
 ]
 
