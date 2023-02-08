885a3a5064cdef0fedcea815d8dea9b642edbe64
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2023-02-08 16:45:23 +0530

    Update teleutils.py

--- ethon/teleutils.py
+++ ethon/teleutils.py
@@ -18,7 +18,4 @@ from telethon import events
 
 #to mention 
 async def mention(bot, id):
-    a = await bot.get_entity(int(id))
-    x = a.first_name
-    return f'[{x}](tg://user?id={id})'
-
+    break
