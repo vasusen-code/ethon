9870e60cc0d50c861419b662ef9f795824a17f91
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2023-03-09 20:34:13 +0530

    Update pyfunc.py

--- ethon/pyfunc.py
+++ ethon/pyfunc.py
@@ -1,15 +1,12 @@
 """This file is part of the ethon distribution.
 Copyright (c) 2021 vasusen-code
-
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, version 3.
-
 This program is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 General Public License for more details.
-
 License can be found in < https://github.com/vasusen-code/ethon/blob/main/LICENSE > ."""
 
 #vasusen-code/thechariotoflight/dronebots
@@ -24,6 +21,20 @@ def total_frames(video_path):
     tf = int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) 
     return tf        
 
+#makes a subprocess handy
+def bash(cmd):    
+    bashCommand = f"{cmd}"
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE) 
+    output, error = process.communicate()
+    return output, error
+
 #to get width, height and duration(in sec) of a video
 def video_metadata(file):
-    break
+    vcap = cv2.VideoCapture(f'{file}')  
+    width = round(vcap.get(cv2.CAP_PROP_FRAME_WIDTH ))
+    height = round(vcap.get(cv2.CAP_PROP_FRAME_HEIGHT ))
+    fps = vcap.get(cv2.CAP_PROP_FPS)
+    frame_count = vcap.get(cv2.CAP_PROP_FRAME_COUNT)
+    duration = round(frame_count / fps)
+    data = {'width' : width, 'height' : height, 'duration' : duration }
+    return data
