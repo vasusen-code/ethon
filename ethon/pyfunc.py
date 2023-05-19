fad4367acde1ae24844a61bc9c02ad5747f5fe06
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2023-05-19 18:49:29 +0530

    Update pyfunc.py

--- ethon/pyfunc.py
+++ ethon/pyfunc.py
@@ -11,8 +11,7 @@ License can be found in < https://github.com/vasusen-code/ethon/blob/main/LICENS
 
 #vasusen-code/thechariotoflight/dronebots
 #__TG:ChauhanMahesh__
-
-import subprocess 
+ 
 import cv2
 
 #fastest way to get total number of frames in a video
@@ -39,6 +38,37 @@ def videometadata(file):
     data = {'width' : width, 'height' : height, 'duration' : duration }
     return data
 
+# function to find the resolution of the input video file
+
+import subprocess
+import shlex
+import json
+
+def findVideoResolution(pathToInputVideo):
+    cmd = "ffprobe -v quiet -print_format json -show_streams"
+    args = shlex.split(cmd)
+    args.append(pathToInputVideo)
+    # run the ffprobe process, decode stdout into utf-8 & convert to JSON
+    ffprobeOutput = subprocess.check_output(args).decode('utf-8')
+    ffprobeOutput = json.loads(ffprobeOutput)
+
+    # find height and width
+    height = ffprobeOutput['streams'][0]['height']
+    width = ffprobeOutput['streams'][0]['width']
+
+    # find duration
+    out = subprocess.check_output(["ffprobe", "-v", "quiet", "-show_format", "-print_format", "json", pathToInputVideo])
+    ffprobe_data = json.loads(out)
+    duration_seconds = float(ffprobe_data["format"]["duration"])
+
+    return int(height), int(width), int(duration_seconds)
+
+def duration(pathToInputVideo):
+    out = subprocess.check_output(["ffprobe", "-v", "quiet", "-show_format", "-print_format", "json", pathToInputVideo])
+    ffprobe_data = json.loads(out)
+    duration_seconds = float(ffprobe_data["format"]["duration"])
+    return int(duration_seconds)
+  
 def video_metadata(file):
     height = 720
     width = 1280
